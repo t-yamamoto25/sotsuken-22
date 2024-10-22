a3f5d2032a92ff37b602a4c66ffd11f0a0dd5834
yamamoto <166352818+t-yamamoto25@users.noreply.github.com>
2024-10-22 09:31:48 +0900

    Add files via upload

--- sotsuken/login_process.php
+++ sotsuken/login_process.php
@@ -3,7 +3,7 @@ session_start();
 
 // 仮のユーザー名とパスワード
 $valid_username = 'k22rs222';
-$valid_password = 'ksu222';
+$valid_password = 'ksu22222';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
