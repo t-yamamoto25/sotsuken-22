3a040922efe315b1ab86cb130b33c99607193f22
Takatsuguyamamoto22149 <k22rs149@st.kyusan-u.ac.jp>
2024-10-31 08:59:04 +0900

    更新

--- sotsuken/config.php
+++ sotsuken/config.php
@@ -1,18 +1,12 @@
-<?php
-// データベース接続設定
-$servername = "localhost";
-$username = "root"; // データベースのユーザー名
-$password = ""; // データベースのパスワード
-$dbname = "sotsuken22"; // データベース名
+<?php 
+// MySQLサーバへ接続
+$conn = new mysqli("localhost", "root", "", "sotsuken22");
 
-// データベースへの接続を確立
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 接続をチェック
-if ($conn->connect_error) {
+// 接続エラーチェック
+if ($conn->connect_errno) {
     die("データベース接続に失敗しました: " . $conn->connect_error);
 }
 
-// 文字コード設定
-$conn->set_charset("utf8");
+// 文字コードをutf8に設定（文字化け対策）
+$conn->set_charset('utf8');
 ?>
