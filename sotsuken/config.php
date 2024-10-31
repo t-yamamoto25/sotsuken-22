c719a815ad31e9cc759869568eef255c24682375
Takatsuguyamamoto22149 <k22rs149@st.kyusan-u.ac.jp>
2024-10-31 09:23:50 +0900

    更新

--- sotsuken/config.php
+++ sotsuken/config.php
@@ -1,12 +1,5 @@
 <?php 
-// MySQLサーバへ接続
-$conn = new mysqli("localhost", "root", "", "sotsuken22");
-
-// 接続エラーチェック
-if ($conn->connect_errno) {
-    die("データベース接続に失敗しました: " . $conn->connect_error);
-}
-
-// 文字コードをutf8に設定（文字化け対策）
-$conn->set_charset('utf8');
+  $conn = new mysqli("localhost","root","", "sotsuken22");//MySQLサーバへ接続
+  if ($conn->connect_errno) die($conn->connect_error);
+  $conn->set_charset('utf8'); //文字コードをutf8に設定（文字化け対策）
 ?>
