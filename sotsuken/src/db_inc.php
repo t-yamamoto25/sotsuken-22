feaa79fe922fb4bcbb95343366e586f2f40a6943
yamamoto <166352818+t-yamamoto25@users.noreply.github.com>
2024-11-12 10:37:21 +0900

    Delete sotsuken directory

--- sotsuken/src/db_inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$conn = new mysqli("localhost", "root", "", "wp2024");//＜開発時の環境設定＞
-$conn = new mysqli("localhost", "root", " ", "sotsuken22"); //＜運用時の環境設定＞
-if ($conn->connect_errno) die($conn->connect_error);
-$conn->set_charset('utf8'); //文字コードをutf8に設定（文字化け対策）
--- sotsuken/src/login.php
+++
@@ -1,9 +0,0 @@
-<h2>ログイン</h2>
-<form action="?do=check" method="post" class="form">
-<table class="table table-hover">
-<tr><td>ユーザ名：</td><td><input type="text" name="uid" class="form-control"></td></tr>
-<tr><td>パスワード：</td><td><input type="password" name="pass" class="form-control"></td></tr>
-</table>
-<input type="submit" value="送信" class="btn btn-primary">
-<input type="reset" value="取消" class="btn btn-danger">
-</form>
